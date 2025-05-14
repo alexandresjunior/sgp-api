f8a4a43e9192bce3821c67d4f091d13e58e0fb69
Alexandre Junior <souzajr.alexandre@gmail.com>
2025-05-14 20:34:14 -0300

    Uso do ResponseEntity para definir status da resposta

--- src/main/java/com/sgp/api/controller/UsuarioController.java
+++ src/main/java/com/sgp/api/controller/UsuarioController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,13 +16,19 @@ import com.sgp.api.service.UsuarioService;
 public class UsuarioController {
 
     @GetMapping("/usuarios")
-    public List<Usuario> listarUsuarios() {
-        return usuarioService.carregarUsuariosCadastrados();
+    public ResponseEntity<List<Usuario>> listarUsuarios() {
+        return ResponseEntity.ok().body(usuarioService.carregarUsuariosCadastrados());
     }
 
     @GetMapping("/usuarios/{id}")
-    public Optional<Usuario> buscarUsuarioPeloId(@PathVariable("id") Long id) {
-        return usuarioService.obterDadosUsuarioPeloId(id);
+    public ResponseEntity<Optional<Usuario>> buscarUsuarioPeloId(@PathVariable("id") Long id) {
+        Optional<Usuario> usuario = usuarioService.obterDadosUsuarioPeloId(id);
+
+        if (usuario.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok().body(usuario);
     }
     
     @Autowired
